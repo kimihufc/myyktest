0b4a680a24f0b4b9ecc1f810fefab3450f3cf350
hujianglai <11111111>
2020-09-27 16:57:05 +0800

    DeferredResult 应用

--- src/main/java/controller/SwitchController.java
+++ src/main/java/controller/SwitchController.java
@@ -3,11 +3,13 @@ package controller;
 import bean.SysSwitch;
 import dao.SysSwitchMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
 
@@ -31,5 +33,32 @@ public class SwitchController {
         SysSwitch param = new SysSwitch();
         param.setId(id);
         return sysSwitchMapper.selectOne(param);
+    }
+
+    /**
+     * DeferredResult 只要有值就会等主程序执行完返回结果 如果一直没有设置值 等超时就会返回设置的超时值
+     * @param time
+     * @return
+     */
+    @RequestMapping(value = "test")
+    @ResponseBody
+    public DeferredResult<String> testDeferedResult(Long time){
+        DeferredResult<String> result = new DeferredResult<>(3000L,"time over");
+        result.onTimeout(()->{
+//            result.setResult("timeout ...");
+            System.out.println("time over");
+        });
+        result.setResultHandler(new DeferredResult.DeferredResultHandler() {
+            @Override
+            public void handleResult(Object o) {
+                System.out.println("begin ....");
+
+                System.out.println("over ...");
+            }
+        });
+        if(time>2000){
+            result.setResult("success");
+        }
+        return result;
     }
 }
